48ca470bfcbe53ad3b585048ee42351d9f29f966
BrentD09 <brentdenny@gmail.com>
2018-03-23 15:38:54 +1000

    fixed the LCM file

--- ConfigLCMAsPullNode.ps1
+++ ConfigLCMAsPullNode.ps1
@@ -1,2 +1,17 @@
 # This needs to be run on
-[DSCLocalConfigurationManager()]configuration PullClientConfig{    Node localhost    {        Settings        {            RefreshMode = 'Pull'            ConfigurationID = '1d545e3b-60c3-47a0-bf65-5afc05182fd0'            RefreshFrequencyMins = 30             RebootNodeIfNeeded = $true        }        ConfigurationRepositoryWeb LON-DC1        {            ServerURL = 'http://LON-DC1:8080/PSDSCPullServer.svc'        }          }}PullClientConfigIDSet-DscLocalConfigurationManager localhost -path .\PullClientConfig 
+[DSCLocalConfigurationManager()]
+configuration PullClientConfig {
+  Node localhost {
+    Settings {
+      RefreshMode = 'Pull'
+      ConfigurationID = '1d545e3b-60c3-47a0-bf65-5afc05182fd0'
+      RefreshFrequencyMins = 30 
+      RebootNodeIfNeeded = $true
+    }
+    ConfigurationRepositoryWeb LON-DC1 {
+      ServerURL = 'http://LON-DC1:8080/PSDSCPullServer.svc
+    }      
+  }
+}
+PullClientConfigID
+Set-DscLocalConfigurationManager localhost -path .\PullClientConfig 
