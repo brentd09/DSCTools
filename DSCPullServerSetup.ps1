893abc6fbb64cb498be322eb196bd714ce1b71f5
BrentD09 <brentdenny@gmail.com>
2018-03-23 14:46:21 +1000

    Create DSCPullServerSetup script

---
+++ DSCPullServerSetup.ps1
@@ -0,0 +1,55 @@
+try {
+  Invoke-WebRequest -Uri  "http://localhost:8080/PSDSCPullServer.svc/" -ErrorAction stop | Out-Null
+} 
+Catch {
+  $InternetIsConnected = Test-NetConnection -ComputerName '8.8.8.8' -InformationLevel Quiet
+  if ($InternetIsConnected) {
+    Install-Module xPsDesiredStateConfiguration -Force -AllowClobber
+    Install-WindowsFeature DSC-Service -IncludeAllSubFeature -IncludeManagementTools 
+    Enable-PSRemoting -Force -SkipNetworkProfileCheck 
+    
+    Configuration DSCPullServerConfig {
+      param (
+        [string[]]$ComputerName = 'localhost'
+      )
+      Import-DscResource -ModuleName xPsDesiredStateConfiguration
+      Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+      node $ComputerName {
+        WindowsFeature DSCServiceFeature {
+          Ensure = "Present"
+          Name = "DSC-Service"
+        }
+
+        xDscWebService PSDSCPullServer {
+          Ensure = "Present"
+          EndpointName = "PSDSCPullServer"
+          Port = 8080
+          PhysicalPath = "$env:systemdrive\inetpub\wwwroot\PSDSCPullServer"
+          CertificateThumbPrint = "AllowUnencryptedTraffic"
+          ModulePath = "$env:programfiles\windowspowershell\DSCService\Modules"
+          ConfigurationPath = "$env:programfiles\windowspowershell\DSCService\Configuration"
+          State = "Started"
+          DependsOn = "[WindowsFeature]DSCServiceFeature"
+          UseSecurityBestPractices = $false
+        }
+
+        xDscWebService PSDSCReportServer {
+          Ensure = "Present"
+          EndpointName = "PSDSCReportServer"
+          Port = 9080
+          PhysicalPath = "$env:systemdrive\inetpub\wwwroot\PSDSCReportServer"
+          State = "Started"
+          DependsOn = ("[WindowsFeature]DSCServiceFeature","[xDscWebService]PSDSCPullServer")
+          CertificateThumbPrint = "AllowUnencryptedTraffic"
+          UseSecurityBestPractices = $false
+        }  
+      }
+    } 
+    DSCPullServerConfig
+    Start-DscConfiguration -Path .\DSCPullServerConfig -Force -Wait -Verbose
+    
+    try {[xml]$PullServerXML = Invoke-WebRequest -Uri  "http://localhost:8080/PSDSCPullServer.svc/" -ErrorAction stop} 
+    Catch {Write-Warning "Pull Server is not working"}
+  } #End if
+} #End Catch
